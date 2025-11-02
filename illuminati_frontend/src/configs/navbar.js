04b5df87edf6497ff3ef1ddbf60053473b48979a
obulka <oleg.bulka@gmail.com>
2025-11-02 12:08:48 +0200

    TAT-54: connect hall of fame to navbar

--- illuminati_frontend/src/configs/navbar.js
+++ illuminati_frontend/src/configs/navbar.js
@@ -11,6 +11,7 @@ export const navItems = [
   },
   { name: "Invite", roles: ["GoldMason", "Architect"], href: "/invite" },
   { name: "Broadcast Emails", roles: ["Architect"], href: "/broadcast" },
+  { name: "Hall of Fame", roles: ["Architect"], href: "/hall_of_fame" },
   { name: "Download/Upload", roles: ["Architect"], href: "/records" },
   {
     name: "ProtectedHome",
