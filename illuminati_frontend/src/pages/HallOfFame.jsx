852343cf7ebaeee2f4aa9de156f20e5671f284b7
obulka <oleg.bulka@gmail.com>
2025-11-02 22:54:13 +0200

    TAT-54: handle and log error in HallOfFame message sending

--- illuminati_frontend/src/pages/HallOfFame.jsx
+++ illuminati_frontend/src/pages/HallOfFame.jsx
@@ -29,7 +29,9 @@ export default function HallOfFame() {
       setMessage("");
       setSelected("");
     } catch (e) {
+      console.error("Failed to send message", e);
       setStatus("error");
+      alert("An error occurred while sending your message. Please try again.");
     }
   };
 
